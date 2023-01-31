19146b553a80c3de9f8377237bf78cda626ca5ab
Prateek <prateek2123@gmail.com>
2023-01-31 22:07:50 +0530

    cards to be made

--- lib/card.dart
+++ lib/card.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
-class Card extends StatelessWidget {
-  const Card({Key? key}) : super(key: key);
+class ListCard extends StatelessWidget {
+  String address;
+  ListCard(this.address);
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect();
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+            onTap: () {},
+            child: Image.asset(
+              address,
+              fit: BoxFit.cover,
+            )),
+        footer: const GridTileBar(
+          title: Text('title'),
+          backgroundColor: Colors.black87,
+        ),
+      ),
+    );
   }
 }
--- lib/programs.dart
+++ lib/programs.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
 
+import 'card.dart';
+
 class Programs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+        padding: EdgeInsets.all(4),
         child: Column(
-      children: [
-        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          const Text('Programs for you'),
-          Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [Text("View all"), Icon(Icons.arrow_forward)])
-        ]),
-        // ListView(
-        //   scrollDirection: Axis.horizontal,
-        //   children: [],
-        // )
-        ClipRRect(
-          child: Image.asset('assets/images/mother1.jpg'),
-          borderRadius: BorderRadius.circular(5),
-        )
-      ],
-    ));
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              const Text(
+                'Programs for you',
+                style: TextStyle(fontSize: 20),
+              ),
+              Row(mainAxisSize: MainAxisSize.min, children: const [
+                Text(
+                  "View all",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Icon(Icons.arrow_forward)
+              ])
+            ]),
+            const SizedBox(
+              height: 30,
+            ),
+            // ListView(
+            //   scrollDirection: Axis.horizontal,
+            //   children: [],
+            // )
+            ListCard("assets/images/mother1.jpg")
+          ],
+        ));
   }
 }
